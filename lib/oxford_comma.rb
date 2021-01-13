e52f213ec026ab4094dc6657f1846be145335591
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-13 11:26:11 -0500

    commit tree 3a4a6e97d45967b6e12630eebdf573fc30c5556f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size() == 1
+    array[0]
+  elsif array.size() == 2
+    return two = array.join(" and ")
+  else
+    last = array.pop()
+    oxford = array.join(", ")
+    oxford += ", and #{last}"
+  end
 end
